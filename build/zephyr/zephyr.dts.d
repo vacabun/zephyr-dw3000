fe02107673dfdda25f7392a6fde8c45daa662d27
vacabun <maguotong66@gmail.com>
2022-11-09 16:19:42 +0800

    update

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,19 @@
+empty_file.o: /mnt/d/code/zephyrproject/zephyr/misc/empty_file.c \
+ /mnt/d/code/zephyrproject/zephyr/boards/arm/my_stm32f401/my_stm32f401.dts \
+ /mnt/d/code/zephyrproject/zephyr/dts/arm/st/f4/stm32f401Xc.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/dts/common/mem.h \
+ /mnt/d/code/zephyrproject/zephyr/dts/arm/st/f4/stm32f401.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/dts/arm/st/f4/stm32f4.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/stm32f4_clock.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/stm32_pwm.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/sensor/qdec_stm32.h \
+ /mnt/d/code/zephyrproject/zephyr/dts/common/freq.h \
+ /mnt/d/code/zephyrproject/modules/hal/stm32/dts/st/f4/stm32f401c(b-c)ux-pinctrl.dtsi \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl.h \
+ /mnt/d/code/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl-common.h \
+ /mnt/d/code/zephyrproject/zephyr-dw3000/boards/my_stm32f401.overlay
---
+++ src/main.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+#include "dw3000_hw.h"
+#include "deca_probe_interface.h"
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+	     "Console device is not ACM CDC UART device");
+
+void main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+
+    dw3000_hw_init();
+    dw3000_hw_reset();
+    dw3000_hw_init_interrupt();
+    dw3000_spi_speed_fast();
+
+    int ret = dwt_probe((struct dwt_probe_s*)&dw3000_probe_interf);
+
+	while (1) {
+		if (ret < 0) {
+            printk("DWT Probe failed");
+        }
+        else{
+            printk("DWT Probe Succeed");
+        }
+		k_sleep(K_SECONDS(1));
+	}
+}
